d3959eb149e8e7b31f2b4899c397b245651e4e44
EG1129 <152231518+EG1129@users.noreply.github.com>
2025-12-11 18:39:29 -0500

    Implement GPU execution time measurement
    Added timing functionality to measure GPU execution time.

--- matrixMultiplication_CUDA.cu
+++ matrixMultiplication_CUDA.cu
@@ -4,11 +4,14 @@
 #include <ctime>
 #include <iomanip>
 #include <cuda_runtime.h>
+#include <chrono>
 
 using namespace std;
+using namespace std::chrono;
 
+// GPU KERNEL
 __global__ void matMulKernel(const double* A, const double* B, double* C,
-                             int r, int c, int c1)
+    int r, int c, int c1)
 {
     int r_idx = blockIdx.y * blockDim.y + threadIdx.y;   // row of A, row of C
     int c1_idx = blockIdx.x * blockDim.x + threadIdx.x;  // column of B, column of C
@@ -17,21 +20,21 @@ __global__ void matMulKernel(const double* A, const double* B, double* C,
     {
         double sum = 0.0;
 
-        for (int t = 0; t < c; ++t)   // shared dimension: A's c, B's r1
+        for (int t = 0; t < c; ++t)   
         {
             int A_index = r_idx * c + t;       // A[r_idx][t]
             int B_index = t * c1 + c1_idx;     // B[t][c1_idx]
             sum += A[A_index] * B[B_index];
         }
 
-        C[r_idx * c1 + c1_idx] = sum;          // C[r][c1]
+        C[r_idx * c1 + c1_idx] = sum;
     }
 }
 
 int main()
 {
-    int r, c;       // A: r x c
-    int r1, c1;     // B: r1 x c1
+    int r, c;       // A dimensions
+    int r1, c1;     // B dimensions
 
     cout << "Enter rows and columns of matrix A (r c): ";
     cin >> r >> c;
@@ -52,35 +55,14 @@ int main()
     for (int i = 0; i < r * c; ++i) h_A[i] = rand() % 10;
     for (int i = 0; i < r1 * c1; ++i) h_B[i] = rand() % 10;
 
-    // Print A, can comment this out for faster results
-    cout << "\nMatrix A (" << r << " x " << c << "):\n";
-    for (int i = 0; i < r; ++i)
-    {
-        for (int j = 0; j < c; ++j)
-        {
-            cout << h_A[i * c + j] << " ";
-        }
-        cout << "\n";
-    }
-
-    // Print B, can comment this out for faster results
-    cout << "\nMatrix B (" << r1 << " x " << c1 << "):\n";
-    for (int i = 0; i < r1; ++i)
-    {
-        for (int j = 0; j < c1; ++j)
-        {
-            cout << h_B[i * c1 + j] << " ";
-        }
-        cout << "\n";
-    }
-    cout << endl;
-
     // Allocate device memory
-    double *d_A, *d_B, *d_C;
-
+    double* d_A, * d_B, * d_C;
     cudaMalloc(&d_A, sizeA);
     cudaMalloc(&d_B, sizeB);
     cudaMalloc(&d_C, sizeC);
+
+    
+    auto start = high_resolution_clock::now();
 
     cudaMemcpy(d_A, h_A.data(), sizeA, cudaMemcpyHostToDevice);
     cudaMemcpy(d_B, h_B.data(), sizeB, cudaMemcpyHostToDevice);
@@ -88,20 +70,26 @@ int main()
     dim3 blockDim(16, 16);
     dim3 gridDim((c1 + 15) / 16, (r + 15) / 16);
 
-    matMulKernel<<<gridDim, blockDim>>>(d_A, d_B, d_C, r, c, c1);
+    matMulKernel << <gridDim, blockDim >> > (d_A, d_B, d_C, r, c, c1);
+    cudaDeviceSynchronize(); // ensure kernel completed
 
     cudaMemcpy(h_C.data(), d_C, sizeC, cudaMemcpyDeviceToHost);
 
-    // print result matrix C
-    cout << "Result matrix C (" << r << " x " << c1 << "):\n";
-    for (int i = 0; i < r; ++i)
-    {
-        for (int j = 0; j < c1; ++j)
-        {
-            cout << h_C[i * c1 + j] << " ";
-        }
-        cout << "\n";
-    }
+    auto end = high_resolution_clock::now();
+
+    double total_time = duration<double, milli>(end - start).count();
+    cout << "\nTotal GPU Execution Time: " << total_time << " ms\n\n";
+
+    //// Print C
+    //cout << "Result matrix C (" << r << " x " << c1 << "):\n";
+    //for (int i = 0; i < r; ++i)
+    //{
+    //    for (int j = 0; j < c1; ++j)
+    //    {
+    //        cout << h_C[i * c1 + j] << " ";
+    //    }
+    //    cout << "\n";
+    //}
 
     cudaFree(d_A);
     cudaFree(d_B);
@@ -109,6 +97,7 @@ int main()
 
     return 0;
 }
+
 
 
 
